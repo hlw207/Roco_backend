85d9771905b4cc7e8357eb08a7f1e0ca3e2cdc8b
HeLiwei <211250207@smail.nju.edu.cn>
2024-10-15 13:10:33 +0800

    change database sourse

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,12 @@ name="roco_backend"
 docker container stop ${name}
 docker rm ${name}
 docker rmi ${name}:latest
-
 echo "卸载缓存成功"
+
+mvn package -DskipTests
+echo "构建成功"
 
 docker build -t ${name} .
 echo "镜像构建成功"
 
-docker run -d -p 8081:8081 --name ${name} ${name}
+docker run -d -p 8081:8081 --restart=always --name ${name} ${name}
