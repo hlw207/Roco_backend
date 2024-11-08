0a3de71f0da3424b73b967683046a8d8d7f90b79
HeLiwei <211250207@smail.nju.edu.cn>
2024-11-08 17:09:15 +0800

    update

--- src/main/java/com/example/roco_backend/Controller/UserController.java
+++ src/main/java/com/example/roco_backend/Controller/UserController.java
@@ -22,7 +22,8 @@ public class UserController {
 
     @GetMapping("/register")
     public boolean register(@RequestParam String account, @RequestParam String password){
-        return userService.register(account, password);
+        return false;
+//        return userService.register(account, password);
     }
 
     @GetMapping("/name")
